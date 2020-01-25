a930a6ad543770a9190897b978b67677025bd4fd
aserlop <alvaro.serrano@cunef.edu>
2020-01-25 12:27:48 +0100

    model ready

---
+++ bin/train_hyper.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+gcloud ai-platform jobs submit training generator_train_ht_`date +"%s"` \
+  --python-version=3.7 \
+  --runtime-version=1.15 \
+  --scale-tier basic \
+  --package-path ./trainer \
+  --module-name trainer.task \
+  --job-dir=gs://ihr-kschool-training/tmp/ \
+  --region us-central1 \
+  --config ./bin/hyper.yaml \
+  -- \
+  --download \
+  --bucket-name kschool-challenge-vcm \
+  --prefix data/dogs_cats/
