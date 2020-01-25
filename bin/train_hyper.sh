6f51b5acddad3cbf1ba98310e47805466eca4ca3
aserlop <alvaro.serrano@cunef.edu>
2020-01-25 12:46:21 +0100

    reduce resources

--- bin/train_hyper.sh
+++ bin/train_hyper.sh
@@ -3,10 +3,10 @@
 gcloud ai-platform jobs submit training generator_train_ht_`date +"%s"` \
   --python-version=3.7 \
   --runtime-version=1.15 \
-  --scale-tier basic \
+  --scale-tier BASIC \
   --package-path ./trainer \
   --module-name trainer.task \
-  --job-dir=gs://ihr-kschool-training/tmp/ \
+  --job-dir=gs://asl-kschool-training/tmp/ \
   --region us-central1 \
   --config ./bin/hyper.yaml \
   -- \
